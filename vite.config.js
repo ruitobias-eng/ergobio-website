335d96b7a06320ffa6743af9c51c5ab5ce18deb9
ruitobias-eng <ruitobiascarvalho@hotmail.com>
2025-10-08 14:32:24 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/ergobio-website/',  // Defina o nome do seu repositório aqui
 })
